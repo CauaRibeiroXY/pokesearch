f4d5c28d6f251987f64f316f8a242d67c21ff528
Cauã Ribeiro <cauaqwertyuiopasdfghjkl@gmail.com>
2021-12-14 19:45:32 -0300

    Ajustes css, implementação da busca pela especie

--- public/functions.js
+++ public/functions.js
@@ -5,42 +5,48 @@ const shiny = document.getElementById('shiny')
 const statuspkn = document.getElementById('statuspkn')
 const pkmimage = document.getElementById('pkmimage')
 const button = document.getElementById('buscar')
+const species_name = document.getElementById('species')
 
+const random = document.getElementById('random')
 
 button.addEventListener('click', () => {
     valor = input.value
-    valor=valor.toLowerCase()
+    valor = valor.toLowerCase()
     buscar(valor)
+    
 
 })
 
-const random = document.getElementById('random')
+
 random.addEventListener('click', () => {
 
     buscar(getRandomInt(1, 898))
     
 })
 
+
+
 function buscar(valor) {
-    
+
     if (valor > 898)
         valor = 898
 
     if (valor < 1)
         valor = 1
-
+        pkn_species(valor)
 
     link = baseUrl + "pokemon/" + valor
-    
-    if (!shiny.checked){
+
+    if (!shiny.checked) {
         fetch(link)
             .then((response) => response.json())
             .then((data) => {
-                namepkm.textContent = data.name +" #" + data.id
+                namepkm.textContent = data.name + " #" + data.id
+                
                 pkmimage.style.backgroundImage = `url('${data.sprites.other.home.front_default}')`
                 statuspkn.innerHTML = ''
                 for (var i in data.stats) {
-                    
+
                     statuspkn.innerHTML +=
                         `<li>
                 ${data.stats[i].stat.name}: ${data.stats[i].base_stat}
@@ -55,16 +61,54 @@ function buscar(valor) {
                 pkmimage.style.backgroundImage = ''
                 statuspkn.innerHTML = ''
             })
-        }else{
+    } else {
         fetch(link)
-        .then((response) => response.json())
-        .then((data) => {
-            namepkm.textContent = data.name +" #" + data.id
-            pkmimage.style.backgroundImage = `url('${data.sprites.other.home.front_shiny}')`
-        })
-        }
+            .then((response) => response.json())
+            .then((data) => {
+                
+                
+                namepkm.textContent = data.name + " #" + data.id
+                pkmimage.style.backgroundImage = `url('${data.sprites.other.home.front_shiny}')`
+            })
+    }
 
 }
+
+function pkn_species(id) {
+
+    link = baseUrl + "pokemon-species/" + id
+
+
+    fetch(link)
+        .then((response) => response.json())
+        .then((data) => {
+            species_name.innerHTML = ''
+
+            for (var i in data.varieties) {
+
+                species_name.innerHTML +=
+                    `<button class="especies" type="button" style = "width:50%" value="${data.varieties[i].pokemon.name}">
+                    ${data.varieties[i].pokemon.name}
+                    </button>`
+
+            }
+            var variavelspecieis = document.getElementsByClassName("especies")
+
+            for (var i in variavelspecieis) {
+
+                console.log(variavelspecieis[i].attributes.value.value)
+                
+                    variavelspecieis[i].addEventListener('click', (e) => {
+
+                        console.log(e.currentTarget.value)
+                        buscar(e.currentTarget.value)
+
+                    })
+            }
+        })
+
+}
+
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
